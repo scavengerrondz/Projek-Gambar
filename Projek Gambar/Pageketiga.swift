46a845c7930e054f30fc95c11c43a0ce20ce9ff5
Zaim Zawawi <mzaimzawawi@gmail.com>
2023-08-20 03:57:33 +0800

    makan

---
+++ Projek Gambar/Pageketiga.swift
@@ -0,0 +1,20 @@
+//
+//  Pageketiga.swift
+//  Projek Gambar
+//
+//  Created by Mohd Zaim Bin Abdullah Zawawi on 20/08/2023.
+//
+
+import SwiftUI
+
+struct Pageketiga: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pageketiga_Previews: PreviewProvider {
+    static var previews: some View {
+        Pageketiga()
+    }
+}
---
+++ Projek Gambar/SwiftUIView.swift
@@ -0,0 +1,26 @@
+//
+//  SwiftUIView.swift
+//  Projek Gambar
+//
+//  Created by Mohd Zaim Bin Abdullah Zawawi on 20/08/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        VStack{
+            Image ("mspuiyi2")
+            
+            Text("Miss Puiyi hisap konek")
+            
+        }
+   
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
