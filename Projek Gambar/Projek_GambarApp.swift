acc6837f49a91b17ac2a0653af18911fc182e365
Zaim Zawawi <mzaimzawawi@gmail.com>
2023-08-20 03:43:31 +0800

    Initial Commit

---
+++ Projek Gambar/Projek_GambarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projek_GambarApp.swift
+//  Projek Gambar
+//
+//  Created by Mohd Zaim Bin Abdullah Zawawi on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Projek_GambarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
